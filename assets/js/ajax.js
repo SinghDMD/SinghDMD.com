c926c65075c00d6df1eced0767382ede6bdb8a82
Gursharanjit Singh <462983+SinghDMD@users.noreply.github.com>
2025-09-06 14:05:58 -0400

    Changed

--- assets/js/ajax.js
+++ assets/js/ajax.js
@@ -1,4 +1,4 @@
 $(document).ready(function(){
     $("footer").addClass("footer py-4 bg-dark text-light text-center");
-    $("footer").load("/assets/include/footer.html");
+    $("footer").load("/assets/include/footer.html"); 
 });
