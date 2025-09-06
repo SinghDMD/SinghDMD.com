f4558a6f3894d8467ec6f149e806a614ce159a59
Gursharanjit Singh <462983+SinghDMD@users.noreply.github.com>
2025-09-06 16:25:02 -0400

    Format

--- assets/js/ajax.js
+++ assets/js/ajax.js
@@ -1,12 +1,12 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
     $("#main-nav").addClass("navbar navbar-expand-lg navbar-light fixed-top");
-     $("#main-nav").load("/assets/include/nav.html");
+    $("#main-nav").load("/assets/include/nav.html");
 
     $("#page-footer").addClass("footer py-4 bg-dark text-light text-center");
-    $("#page-footer").load("/assets/include/footer.html"); 
+    $("#page-footer").load("/assets/include/footer.html");
 
-     $('#page-loader').fadeOut(500, function() {
+    $('#page-loader').fadeOut(500, function () {
         $('body').fadeIn(500);
     });
 });
