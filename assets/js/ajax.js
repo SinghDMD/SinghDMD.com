e227ce6f0d505fb430700bc9e3fcb91a3c8814fc
Gursharanjit Singh <462983+SinghDMD@users.noreply.github.com>
2025-09-06 14:03:33 -0400

    Fix path

--- assets/js/ajax.js
+++ assets/js/ajax.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     $("footer").addClass("footer py-4 bg-dark text-light text-center");
-    $("footer").load("include/footer.html");
+    $("footer").load("/assets/include/footer.html");
 
 
 });
