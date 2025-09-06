c7c4e535c3f79c47f1ab49948c5adc5f81513c12
Gursharanjit Singh <462983+SinghDMD@users.noreply.github.com>
2025-09-06 14:09:29 -0400

    CHangedpaths

--- assets/js/ajax.js
+++ assets/js/ajax.js
@@ -1,4 +1,8 @@
 $(document).ready(function(){
-    $("footer").addClass("footer py-4 bg-dark text-light text-center");
-    $("footer").load("/assets/include/footer.html"); 
+
+    $("#main-nav").addClass("navbar navbar-expand-lg navbar-light fixed-top");
+     $("#main-nav").load("/assets/include/nav.html");
+
+    $("#page-footer").addClass("footer py-4 bg-dark text-light text-center");
+    $("#page-footer").load("/assets/include/footer.html"); 
 });
